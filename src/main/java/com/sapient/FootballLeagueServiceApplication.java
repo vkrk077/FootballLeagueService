65ba9191029d167cbb72e8cb5fb262dda40e8b22
Ravikiran VK <vkrk077@gmail.com>
2020-05-23 19:59:32 +0530

    first commit

---
+++ src/main/java/com/sapient/FootballLeagueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sapient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballLeagueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballLeagueServiceApplication.class, args);
+	}
+
+}
